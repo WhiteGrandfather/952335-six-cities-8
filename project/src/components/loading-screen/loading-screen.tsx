3406e529aa5a0bdae1601f61edefb890bb9d90b2
Mikhail Kolesnichenko <santa_dj@mail.ru>
2021-11-09 21:06:58 +0300

    add new loader

--- project/src/components/loading-screen/loading-screen.tsx
+++ project/src/components/loading-screen/loading-screen.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import './style.css';
+
 export default function LoadingScreen(): JSX.Element {
-  return <p>Loading ...</p>;
+  return (
+    <div className='loader'>
+      <div className='loader__spinner-eclipse'>
+        <div className='loader__spinner'>
+          <div/>
+        </div>
+      </div>
+      <img className='loader__logo'
+        src='img/logo.svg'
+        alt='6 cities logo'
+        width='81'
+        height='41'
+      />
+      <p className="loader__text">Loading ...</p>
+    </div>
+  );
 }
